957a1c4e9c56a9ff1be2c77ac97558fd3203d3c2
prafull <prafullchuahn45@gmail.com>
2020-05-29 19:24:57 +0530

    add

--- scrap.js
+++ scrap.js
@@ -1,3 +1,22 @@
 const request = require('request');
 const cheerio = require('cheerio');
-console.log('hello');
+
+// Making request
+request('https://prafullthakur.netlify.app/', (err, response, html) => {
+    if (!err && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const siteHeading = $('.main-txt');
+        //const output = siteHeading.find('p').text();
+        //const output = siteHeading.children('p').text();
+        //const output = siteHeading.children('p').next().text();
+        //const output = siteHeading.children('p').parent().text();
+        $('.Menu-container a').each((i, lists) => {
+            const list = $(lists).text();
+            const link = $(lists).attr('href');
+            console.log(link);
+        })
+
+    } else {
+        console.log(err);
+    }
+})
